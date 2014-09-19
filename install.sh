9d449d1b686d84423fdf8fbd972658e2d66a449f
Renan Cakirerk <renan@redbeacon.com>
2014-09-19 14:27:00 -0700

    Friendlier install script

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-virtualenv twitcher_env
-source twitcher_env/bin/activate
-pip install -r requirements.txt
-mv config.ini.example config.ini
-./configure.py
-source twitcher_env/bin/activate
-./manage.py syncdb
-./manage.py runserver
+(virtualenv twitcher_env &&
+ source twitcher_env/bin/activate &&
+ pip install -r requirements.txt &&
+ mv config.ini.example config.ini &&
+ ./configure.py &&
+ source twitcher_env/bin/activate &&
+ ./manage.py syncdb &&
+ echo &&
+ echo "--------------------------------" &&
+ echo "Successfully installed Twitcher!" &&
+ echo "--------------------------------" &&
+ echo) ||
+(echo &&
+ echo "--------------------------------" &&
+ echo "      Installation failed!" &&
+ echo "--------------------------------")
