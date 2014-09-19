e40746b902902c9faf7943b63f64fec0fe81ef23
Renan Cakirerk <renan@redbeacon.com>
2014-09-19 02:21:11 -0700

    Change config file name

--- twitcher/settings.py
+++ twitcher/settings.py
@@ -13,7 +13,7 @@ import os
 from os.path import dirname, join
 
 config = configparser.ConfigParser()
-config.read('config.ini.test')
+config.read('config.ini')
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
